9e108e85237b96a144334af1796f42e0b0b68ee5
almasbekkyzyelnura <elnuraalmasbekkyzy8@gmail.com>
2024-03-08 17:38:26 +0600

    тапшыпмаjs 5

---
+++ index.js
@@ -0,0 +1,76 @@
+/* const home = [];
+const streets =['Джал','Ахунбаева', 'Азия Молл','Киевская','Филармония'];
+
+home.push('Джал');
+home.push('Ахунбаева');
+home.push('Азия Молл');
+home.push('Киевская');
+home.push('Филармония');
+console.log(home);
+
+const newArrayHomes = streets.concat(home);
+const arrayConvertString = newArrayHomes.join(',\n ');*/
+
+
+
+
+ const book = [];
+  const arrayOfBooks =('Нежно к себе','Жизнь Пророка Мухаммада', 'Адам болгум келет');
+ 
+  
+  book.push('Нежно к себе');
+  book.push('Жизнь Пророка Мухаммада');
+  book.push('Адам болгум келет');
+  
+  console.log(book.join('\n-'));
+//  console.log(book);
+
+ const newArrayOps = book.join(book);
+const newArrayProducts = arrayOfBooks.concat(book);
+ 
+
+
+
+ 
+
+
+
+ /*const names = [];
+
+ names.push('Марлен');
+ names.push('Арсен');
+ names.push('Ислам');
+ names.push('Сыймык');
+ names.push('Арс');
+
+ const names1 = [];
+
+ names.push('Элнура');
+ names.push('Айбийке');
+ names.push('Азем');
+ names.push('Сада');
+ names.push('Уулсун');
+
+ console.log(names);
+
+ const okuuchular = names.concat(names1);*/
+
+
+
+
+
+
+//let flowers = ['Пиона', 'Роза', 'Сирен', 'Ромашка'];
+
+//for (let i=0; i < flowers.length; i++){
+    //console.log( i + 1 + '- '+ flowers[i]);
+//}
+
+
+
+//const numbers = [1, 2, 3, 4, 5];
+
+//for(let i= 0;  i< numbers.length; i++){
+    //numbers[i] *= 5;
+//}
+//console.log(numbers);
